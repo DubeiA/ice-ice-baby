64a4115546e0717d25ed2ed78395c86b6444d2db
DubeiA <105717456+DubeiA@users.noreply.github.com>
2022-08-10 08:05:56 +0000

    Deploying to gh-pages from @ DubeiA/ice-ice-baby@e8c5aee97ae205347f68164a5566587eea4f0b2a 🚀

---
+++ index.a75a80d7.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("backdrop__is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.a75a80d7.js.map
